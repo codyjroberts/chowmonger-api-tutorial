fe0817cd02c64ca368754502ad10a925a1a54be9
Cody Roberts <codyjroberts@gmail.com>
2016-04-30 13:51:21 -0500

    Remove scrub_params in User Controller. Not used.

--- web/controllers/api/v1/user_controller.ex
+++ web/controllers/api/v1/user_controller.ex
@@ -3,8 +3,6 @@ defmodule Chowmonger.API.V1.UserController do
 
   alias Chowmonger.API.V1.User
 
-  plug :scrub_params, "user" when action in [:create, :update]
-
   def index(conn, _params) do
     users = Repo.all(User)
     render(conn, "index.json", data: users)
