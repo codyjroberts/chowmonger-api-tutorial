2aab34006a3ad77309e13b7fb0a3069c5635e88e
Cody Roberts <codyjroberts@gmail.com>
2016-04-30 11:35:00 -0500

    Remove browser stack, include api stack
    Add version scope

--- web/router.ex
+++ web/router.ex
@@ -13,14 +13,11 @@ defmodule Chowmonger.Router do
     plug :accepts, ["json"]
   end
 
-  scope "/", Chowmonger do
-    pipe_through :browser # Use the default browser stack
+  scope "/api", Chowmonger do
+    pipe_through :api
 
-    get "/", PageController, :index
+    scope "/v1" do
+
+    end
   end
-
-  # Other scopes may use custom stacks.
-  # scope "/api", Chowmonger do
-  #   pipe_through :api
-  # end
 end
